2887a3e5f3176deff6ef3486b6e0cb32b5bc98b6
Michael Ferrer <michaelferrerno@gmail.com>
2018-11-08 16:07:13 -0700

    server is working needs more testing

--- server.js
+++ server.js
@@ -1,30 +1,37 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const expressJwt = require("express-jwt");
 const PORT = process.env.PORT || 5050;
+const app = express()
 
 app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use("/api", expressJwt({ secret: process.env.SECRET }));
 
 mongoose.set("useCreateIndex", true);
-mongoose.connect("mongodb://localhost:27017/ccc-site", 
-    { useNewUrlParser:true },
-    (err) => {
-        if (err) throw err;
-        console.log("connected to the database");
-    });
+mongoose.connect(
+  "mongodb://localhost:27017/ccc-site",
+  { useNewUrlParser: true },
+  err => {
+    if (err) throw err;
+    console.log("connected to the database");
+  }
+);
 
-require("dotenv").config();
-
-const app = express();
-
-app.use(express.json());
-
+app.use("/auth", require("./routes/auth"));
 app.use("/api/cart", require("./routes/cart"));
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, () => console.log(`Connected to MongoDB`))
-app.listen(process.env.PORT, () => console.log(`[+] Starting server on port ${process.env.PORT}`))
+app.use((err, req, res, next) => {
+  console.log(err);
+  if (err.name === "UnauthorizedError") {
+    res.status(err.status);
+  }
+  return res.send({ message: err.message });
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`[+] Starting server on port ${process.env.PORT}`)
+);
